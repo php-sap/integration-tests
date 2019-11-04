6b8e2f69bc644184584a82ecf887b5369218530f
Gregor <gregor-j@users.noreply.github.com>
2019-11-04 11:14:16 +0100

    fix rfc in-out-value assertions of RFC_WALK_THRU_TEST

--- src/AbstractFunctionTestCase.php
+++ src/AbstractFunctionTestCase.php
@@ -126,55 +126,47 @@ abstract class AbstractFunctionTestCase extends AbstractTestCase
         static::assertArrayHasKey('TEST_OUT', $result);
         //create a link for programmer's convenience ...
         $test_out = &$result['TEST_OUT'];
-        foreach ($test_in as $key => $value) {
-            /**
-             * Assert the existence of the key from test IN in test OUT.
-             */
-            static::assertArrayHasKey($key, $test_out, sprintf(
-                'Missing key %s in TEST_OUT!',
-                $key
-            ));
-            if ($key === 'RFCTIME') {
-                /**
-                 * Assert DateTime objects.
-                 */
-                static::assertInstanceOf(\DateTime::class, $test_out[$key], sprintf(
-                    'Test OUT of key %s is not DateTime!',
-                    $key
-                ));
-                //compare only time
-                $format = 'H:i:s';
-                static::assertSame($testDateTime->format($format), $test_out[$key]->format($format));
-            }
-            elseif ($key === 'RFCDATE') {
-                /**
-                 * Assert DateTime objects.
-                 */
-                static::assertInstanceOf(\DateTime::class, $test_out[$key], sprintf(
-                    'Test OUT of key %s is not DateTime!',
-                    $key
-                ));
-                //compare only date
-                $format = 'Y-m-d';
-                static::assertSame($testDateTime->format($format), $test_out[$key]->format($format));
-            } elseif ($key === 'RFCHEX3') {
-                /**
-                 * Assert simple values.
-                 */
-                static::assertSame(hex2bin($value), $test_out[$key], sprintf(
-                    'Test IN and OUT of HEX-key %s don\'t match!',
-                    $key
-                ));
-            } else {
-                /**
-                 * Assert simple values.
-                 */
-                static::assertSame($value, $test_out[$key], sprintf(
-                    'Test IN and OUT of key %s don\'t match!',
-                    $key
-                ));
-            }
-        }
+        /**
+         * Assert float value.
+         */
+        static::assertArrayHasKey('RFCFLOAT', $test_out, 'Missing RFCFLOAT in TEST_OUT!');
+        static::assertSame(70.11, $test_out['RFCFLOAT'], 'Test IN and OUT of RFCFLOAT don\'t match!');
+        /**
+         * Assert integer values.
+         */
+        static::assertArrayHasKey('RFCINT1', $test_out, 'Missing RFCINT1 in TEST_OUT!');
+        static::assertSame(163, $test_out['RFCINT1'], 'Test IN and OUT of RFCINT1 don\'t match!');
+        static::assertArrayHasKey('RFCINT2', $test_out, 'Missing RFCINT2 in TEST_OUT!');
+        static::assertSame(5920, $test_out['RFCINT2'], 'Test IN and OUT of RFCINT2 don\'t match!');
+        static::assertArrayHasKey('RFCINT4', $test_out, 'Missing RFCINT4 in TEST_OUT!');
+        static::assertSame(416639, $test_out['RFCINT4'], 'Test IN and OUT of RFCINT4 don\'t match!');
+        /**
+         * Assert DateTime objects.
+         */
+        static::assertArrayHasKey('RFCTIME', $test_out, 'Missing RFCTIME in TEST_OUT!');
+        static::assertInstanceOf(\DateTime::class, $test_out['RFCTIME'], 'Test OUT of RFCTIME is not DateTime!');
+        static::assertSame($testDateTime->format('H:i:s'), $test_out['RFCTIME']->format('H:i:s'));
+        static::assertArrayHasKey('RFCDATE', $test_out, 'Missing RFCDATE in TEST_OUT!');
+        static::assertInstanceOf(\DateTime::class, $test_out['RFCDATE'], 'Test OUT of RFCDATE is not DateTime!');
+        static::assertSame($testDateTime->format('Y-m-d'), $test_out['RFCDATE']->format('Y-m-d'));
+        /**
+         * Assert hexadecimal value.
+         */
+        static::assertArrayHasKey('RFCHEX3', $test_out, 'Missing RFCHEX3 in TEST_OUT!');
+        static::assertSame('S', $test_out['RFCHEX3'], 'Test IN and OUT of RFCHEX3 don\'t match!');
+        /**
+         * Assert string values.
+         */
+        static::assertArrayHasKey('RFCCHAR1', $test_out, 'Missing RFCCHAR1 in TEST_OUT!');
+        static::assertSame('A', $test_out['RFCCHAR1'], 'Test IN and OUT of RFCCHAR1 don\'t match!');
+        static::assertArrayHasKey('RFCCHAR2', $test_out, 'Missing RFCCHAR2 in TEST_OUT!');
+        static::assertSame('XC', $test_out['RFCCHAR2'], 'Test IN and OUT of RFCCHAR2 don\'t match!');
+        static::assertArrayHasKey('RFCCHAR4', $test_out, 'Missing RFCCHAR4 in TEST_OUT!');
+        static::assertSame('QqMh', $test_out['RFCCHAR4'], 'Test IN and OUT of RFCCHAR4 don\'t match!');
+        static::assertArrayHasKey('RFCDATA1', $test_out, 'Missing RFCDATA1 in TEST_OUT!');
+        static::assertSame('qKWjmNfad32rfS9Z', $test_out['RFCDATA1'], 'Test IN and OUT of RFCDATA1 don\'t match!');
+        static::assertArrayHasKey('RFCDATA2', $test_out, 'Missing RFCDATA2 in TEST_OUT!');
+        static::assertSame('xi82ph2zJ8BCVtlR', $test_out['RFCDATA2'], 'Test IN and OUT of RFCDATA2 don\'t match!');
         /**
          * Assert empty table parameter 'DESTINATIONS'.
          */
